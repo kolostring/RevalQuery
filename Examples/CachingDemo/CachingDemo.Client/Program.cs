667452f3a386805c46eb672f87e0de57dedb7112
kolostring <yoerodriguezjunior@gmail.com>
2026-03-26 18:21:49 -0400

    chore: move examples to own repository for cleaner version control

--- Examples/CachingDemo/CachingDemo.Client/Program.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using RevalQuery.Core;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-builder.Services.AddRevalQuery();
-
-await builder.Build().RunAsync();
